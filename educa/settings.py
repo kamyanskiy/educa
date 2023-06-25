d7797a0c9b82af753c65393debe50330c759e298
Alexander Kamyanskiy <alexander.kamyanskiy@yandex.com>
2023-06-25 23:55:24 +0300

    add DRF with permissions

--- educa/settings.py
+++ educa/settings.py
@@ -33,7 +33,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "embed_video",
     "debug_toolbar",
+    "rest_framework",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
 
 MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
